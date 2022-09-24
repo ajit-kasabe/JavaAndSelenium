10dfa8ffadd6c3075b8f36c38a8e0f978347d75a
ajitkasabe1989 <ajitkasabe@gmail.com>
2022-09-24 12:46:43 +0530

    All files upload

---
+++ src/test/java/ui/LaunchBrowser.java
@@ -0,0 +1,18 @@
+package ui;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LaunchBrowser {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.ebay.com/");
+		driver.manage().window().maximize();
+
+	}
+
+}
